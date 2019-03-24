a372b240bd4833c3cd22d35d524c5220b7434163
anshi <anshiboy@163.com>
2019-03-24 18:25:31 +0800

    测试

--- spring-security-app/src/test/java/com/spring/security/app/SpringSecurityAppApplicationTests.java
+++ spring-security-app/src/test/java/com/spring/security/app/SpringSecurityAppApplicationTests.java
@@ -5,6 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * 这是一个测试类
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringSecurityAppApplicationTests {
